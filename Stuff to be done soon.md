36ab01cadaba6f03cb2162c78946c0d8fa77d7fc
William Sargent <Sp1riting@users.noreply.github.com>
2024-03-13 20:09:24 +0000

    game stats screen improved

---
+++ Stuff to be done soon.md
@@ -0,0 +1,14 @@
+ToDo:
+Two main things left to do in the main game: checking if a full colour set is owned, keep track of the game statistics.
+Other than that, UI and database need to be started
+
+Game things:
+- Add check for full colour set owned when buying houses
+- Add double rent cost when a full colour set is owned
+- Implement selling houses and properties when a player can't afford something
+
+- Check if player can gain rent in jail when paying to somebody else.
+- Add fast bankruptcies
+
+UI Things:
+- Add text to the how to play menu
--- UI/gameStatistics_ui.py
+++ UI/gameStatistics_ui.py
@@ -15,55 +15,111 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDialog, QGridLayout, QGroupBox,
-    QHBoxLayout, QLabel, QLayout, QPushButton,
-    QSizePolicy, QSpacerItem, QWidget)
+from PySide6.QtWidgets import (QApplication, QDialog, QFormLayout, QGridLayout,
+    QGroupBox, QHBoxLayout, QLabel, QLayout,
+    QPushButton, QSizePolicy, QSpacerItem, QWidget)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         if not Dialog.objectName():
             Dialog.setObjectName(u"Dialog")
-        Dialog.resize(950, 372)
+        Dialog.resize(855, 405)
         self.gridLayout_2 = QGridLayout(Dialog)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
+        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.gridLayout.addItem(self.horizontalSpacer_3, 0, 0, 1, 1)
+
         self.groupBox = QGroupBox(Dialog)
         self.groupBox.setObjectName(u"groupBox")
-        self.gridLayout_3 = QGridLayout(self.groupBox)
-        self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.label_3 = QLabel(self.groupBox)
-        self.label_3.setObjectName(u"label_3")
+        self.groupBox.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.formLayout = QFormLayout(self.groupBox)
+        self.formLayout.setObjectName(u"formLayout")
+        self.formLayout.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.formLayout.setLabelAlignment(Qt.AlignCenter)
+        self.formLayout.setFormAlignment(Qt.AlignCenter)
+        self.gamesPlayedLabel = QLabel(self.groupBox)
+        self.gamesPlayedLabel.setObjectName(u"gamesPlayedLabel")
+        font = QFont()
+        font.setPointSize(12)
+        self.gamesPlayedLabel.setFont(font)
 
-        self.gridLayout_3.addWidget(self.label_3, 3, 0, 1, 1)
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.gamesPlayedLabel)
 
-        self.label_5 = QLabel(self.groupBox)
-        self.label_5.setObjectName(u"label_5")
+        self.gamesPlayedValue = QLabel(self.groupBox)
+        self.gamesPlayedValue.setObjectName(u"gamesPlayedValue")
+        self.gamesPlayedValue.setFont(font)
 
-        self.gridLayout_3.addWidget(self.label_5, 2, 0, 1, 1)
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.gamesPlayedValue)
 
-        self.label_6 = QLabel(self.groupBox)
-        self.label_6.setObjectName(u"label_6")
+        self.doublesRolledLabel = QLabel(self.groupBox)
+        self.doublesRolledLabel.setObjectName(u"doublesRolledLabel")
+        self.doublesRolledLabel.setFont(font)
 
-        self.gridLayout_3.addWidget(self.label_6, 1, 0, 1, 1)
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.doublesRolledLabel)
 
-        self.label_2 = QLabel(self.groupBox)
-        self.label_2.setObjectName(u"label_2")
+        self.doublesRolledValue = QLabel(self.groupBox)
+        self.doublesRolledValue.setObjectName(u"doublesRolledValue")
+        self.doublesRolledValue.setFont(font)
 
-        self.gridLayout_3.addWidget(self.label_2, 0, 0, 1, 1)
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.doublesRolledValue)
 
-        self.label_4 = QLabel(self.groupBox)
-        self.label_4.setObjectName(u"label_4")
+        self.propertiesPurchasedLabel = QLabel(self.groupBox)
+        self.propertiesPurchasedLabel.setObjectName(u"propertiesPurchasedLabel")
+        self.propertiesPurchasedLabel.setFont(font)
 
-        self.gridLayout_3.addWidget(self.label_4, 4, 0, 1, 1)
+        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.propertiesPurchasedLabel)
 
-        self.label = QLabel(self.groupBox)
-        self.label.setObjectName(u"label")
+        self.propertiesPurchasedValue = QLabel(self.groupBox)
+        self.propertiesPurchasedValue.setObjectName(u"propertiesPurchasedValue")
+        self.propertiesPurchasedValue.setFont(font)
 
-        self.gridLayout_3.addWidget(self.label, 5, 0, 1, 1)
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.propertiesPurchasedValue)
+
+        self.moneyEarnedLabel = QLabel(self.groupBox)
+        self.moneyEarnedLabel.setObjectName(u"moneyEarnedLabel")
+        self.moneyEarnedLabel.setFont(font)
+
+        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.moneyEarnedLabel)
+
+        self.moneyEarnedValue = QLabel(self.groupBox)
+        self.moneyEarnedValue.setObjectName(u"moneyEarnedValue")
+        self.moneyEarnedValue.setFont(font)
+
+        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.moneyEarnedValue)
+
+        self.rentPaidLabel = QLabel(self.groupBox)
+        self.rentPaidLabel.setObjectName(u"rentPaidLabel")
+        self.rentPaidLabel.setFont(font)
+
+        self.formLayout.setWidget(4, QFormLayout.LabelRole, self.rentPaidLabel)
+
+        self.rentPaidValue = QLabel(self.groupBox)
+        self.rentPaidValue.setObjectName(u"rentPaidValue")
+        self.rentPaidValue.setFont(font)
+
+        self.formLayout.setWidget(4, QFormLayout.FieldRole, self.rentPaidValue)
+
+        self.passedGoLabel = QLabel(self.groupBox)
+        self.passedGoLabel.setObjectName(u"passedGoLabel")
+        self.passedGoLabel.setFont(font)
+
+        self.formLayout.setWidget(5, QFormLayout.LabelRole, self.passedGoLabel)
+
+        self.passedGoValue = QLabel(self.groupBox)
+        self.passedGoValue.setObjectName(u"passedGoValue")
+        self.passedGoValue.setFont(font)
+
+        self.formLayout.setWidget(5, QFormLayout.FieldRole, self.passedGoValue)
 
 
-        self.gridLayout.addWidget(self.groupBox, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.groupBox, 0, 1, 1, 1)
+
+        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.gridLayout.addItem(self.horizontalSpacer_4, 0, 2, 1, 1)
 
 
         self.gridLayout_2.addLayout(self.gridLayout, 2, 0, 1, 1)
@@ -83,9 +139,9 @@ class Ui_Dialog(object):
         sizePolicy.setHeightForWidth(self.gameStatsTitleLabel.sizePolicy().hasHeightForWidth())
         self.gameStatsTitleLabel.setSizePolicy(sizePolicy)
         self.gameStatsTitleLabel.setMaximumSize(QSize(300, 50))
-        font = QFont()
-        font.setPointSize(24)
-        self.gameStatsTitleLabel.setFont(font)
+        font1 = QFont()
+        font1.setPointSize(24)
+        self.gameStatsTitleLabel.setFont(font1)
         self.gameStatsTitleLabel.setLayoutDirection(Qt.LeftToRight)
 
         self.gameStatsHorizontalLayout.addWidget(self.gameStatsTitleLabel)
@@ -100,18 +156,22 @@ class Ui_Dialog(object):
         self.gameStatsReturnButton.setSizePolicy(sizePolicy)
         self.gameStatsReturnButton.setMinimumSize(QSize(200, 50))
         self.gameStatsReturnButton.setMaximumSize(QSize(200, 60))
-        font1 = QFont()
-        font1.setPointSize(16)
-        self.gameStatsReturnButton.setFont(font1)
+        font2 = QFont()
+        font2.setPointSize(16)
+        self.gameStatsReturnButton.setFont(font2)
 
         self.gameStatsHorizontalLayout.addWidget(self.gameStatsReturnButton)
 
 
         self.gridLayout_2.addLayout(self.gameStatsHorizontalLayout, 0, 0, 1, 1)
 
-        self.verticalSpacer = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Fixed)
+        self.verticalSpacer = QSpacerItem(20, 47, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
         self.gridLayout_2.addItem(self.verticalSpacer, 1, 0, 1, 1)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.gridLayout_2.addItem(self.verticalSpacer_2, 3, 0, 1, 1)
 
 
         self.retranslateUi(Dialog)
@@ -122,12 +182,18 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
         self.groupBox.setTitle("")
-        self.label_3.setText(QCoreApplication.translate("Dialog", u"Total Money Earned:", None))
-        self.label_5.setText(QCoreApplication.translate("Dialog", u"Total Properties Purchased:", None))
-        self.label_6.setText(QCoreApplication.translate("Dialog", u"Doubles Rolled:", None))
-        self.label_2.setText(QCoreApplication.translate("Dialog", u"Games Played:", None))
-        self.label_4.setText(QCoreApplication.translate("Dialog", u"Total Rent Paid: ", None))
-        self.label.setText(QCoreApplication.translate("Dialog", u"Number of Times Players Passed Go:", None))
+        self.gamesPlayedLabel.setText(QCoreApplication.translate("Dialog", u"Games Played:", None))
+        self.gamesPlayedValue.setText(QCoreApplication.translate("Dialog", u"0", None))
+        self.doublesRolledLabel.setText(QCoreApplication.translate("Dialog", u"Doubles Rolled:", None))
+        self.doublesRolledValue.setText(QCoreApplication.translate("Dialog", u"0", None))
+        self.propertiesPurchasedLabel.setText(QCoreApplication.translate("Dialog", u"Total Properties Purchased:", None))
+        self.propertiesPurchasedValue.setText(QCoreApplication.translate("Dialog", u"0", None))
+        self.moneyEarnedLabel.setText(QCoreApplication.translate("Dialog", u"Total Money Earned:", None))
+        self.moneyEarnedValue.setText(QCoreApplication.translate("Dialog", u"0", None))
+        self.rentPaidLabel.setText(QCoreApplication.translate("Dialog", u"Total Rent Paid: ", None))
+        self.rentPaidValue.setText(QCoreApplication.translate("Dialog", u"0", None))
+        self.passedGoLabel.setText(QCoreApplication.translate("Dialog", u"Number of Times Players Passed Go:", None))
+        self.passedGoValue.setText(QCoreApplication.translate("Dialog", u"0", None))
         self.gameStatsTitleLabel.setText(QCoreApplication.translate("Dialog", u"Game Statistics", None))
         self.gameStatsReturnButton.setText(QCoreApplication.translate("Dialog", u"Return", None))
     # retranslateUi
