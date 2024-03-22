6f946c9b2fedb4131eb7e680e825343c5ebe2c72
William Sargent <Sp1riting@users.noreply.github.com>
2024-03-22 11:40:44 +0000

    fixed error with capitalisation for class name

--- gameCode.py
+++ gameCode.py
@@ -46,49 +46,49 @@ class GameWindow(QDialog):
         if len(playerNames) >= 1:
             self.player1NameLabel.setText(playerNames[0])
             self.player1BalanceLabel.setText(f"£{startingBalance}")
-            player1 = Players.player(1, playerNames[0], startingBalance)
+            player1 = Players.Player(1, playerNames[0], startingBalance)
             playerList.append(player1)
         
         if len(playerNames) >= 2:
             self.player2NameLabel.setText(playerNames[1])
             self.player2BalanceLabel.setText(f"£{startingBalance}")
-            player2 = Players.player(2, playerNames[1], startingBalance)
+            player2 = Players.Player(2, playerNames[1], startingBalance)
             playerList.append(player2)
 
         if len(playerNames) >= 3:
             self.player3NameLabel.setText(playerNames[2])
             self.player3BalanceLabel.setText(f"£{startingBalance}")
-            player3 = Players.player(3, playerNames[2], startingBalance)
+            player3 = Players.Player(3, playerNames[2], startingBalance)
             playerList.append(player3)
 
         if len(playerNames) >= 4:
             self.player4NameLabel.setText(playerNames[3])
             self.player4BalanceLabel.setText(f"£{startingBalance}")
-            player4 = Players.player(4, playerNames[3], startingBalance)
+            player4 = Players.Player(4, playerNames[3], startingBalance)
             playerList.append(player4)
 
         if len(playerNames) >= 5:
             self.player5NameLabel.setText(playerNames[4])
             self.player5BalanceLabel.setText(f"£{startingBalance}")
-            player5 = Players.player(5, playerNames[4], startingBalance)
+            player5 = Players.Player(5, playerNames[4], startingBalance)
             playerList.append(player5)
 
         if len(playerNames) >= 6:
             self.player6NameLabel.setText(playerNames[5])
             self.player6BalanceLabel.setText(f"£{startingBalance}")
-            player6 = Players.player(6, playerNames[5], startingBalance)
+            player6 = Players.Player(6, playerNames[5], startingBalance)
             playerList.append(player6)
 
         if len(playerNames) >= 7:
             self.player7NameLabel.setText(playerNames[6])
             self.player7BalanceLabel.setText(f"£{startingBalance}")
-            player7 = Players.player(7, playerNames[6], startingBalance)
+            player7 = Players.Player(7, playerNames[6], startingBalance)
             playerList.append(player7)
 
         if len(playerNames) >= 8:
             self.player8NameLabel.setText(playerNames[7])
             self.player8BalanceLabel.setText(f"£{startingBalance}")
-            player8 = Players.player(8, playerNames[7], startingBalance)
+            player8 = Players.Player(8, playerNames[7], startingBalance)
             playerList.append(player8)
 
 
