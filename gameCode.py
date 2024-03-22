ca87b687dc1850676597e7231b09820d7f68abc5
William Sargent <Sp1riting@users.noreply.github.com>
2024-03-22 10:53:10 +0000

    added the additional menus to the main game ui, and have hidden them upon starting the game.

--- gameCode.py
+++ gameCode.py
@@ -8,7 +8,36 @@ class GameWindow(QDialog):
     def __init__(self, username, startingBalance, moneyFromGo, numberOfPlayers, fastBankruptcy, rentFromJail):
         super(GameWindow, self).__init__()
         loadUi('C:\\Users\\willj\\OneDrive\\Documents\\Y13\\coursework\\UI\\game.ui', self)
-        self.chanceCardTextBrowser.hide()
         self.boardImageLabel.setPixmap(QPixmap("C:\\Users\\willj\\OneDrive\\Documents\\Y13\\coursework\\gameBoard.png"))
-        
+
+        self.chanceCardTextBrowser.hide()
+        self.endTurnButton.hide()
+        self.bankruptButton.hide()
+
+        self.dice1Outline.hide()
+        self.dice1dot1.hide()
+        self.dice1dot2.hide()
+        self.dice1dot3.hide()
+        self.dice1dot4.hide()
+        self.dice1dot5.hide()
+        self.dice1dot6.hide()
+        self.dice1dot7.hide()
+
+        self.dice2Outline.hide()
+        self.dice2dot1.hide()
+        self.dice2dot2.hide()
+        self.dice2dot3.hide()
+        self.dice2dot4.hide()
+        self.dice2dot5.hide()
+        self.dice2dot6.hide()
+        self.dice2dot7.hide()
+
+        self.mortgageGroupBox.hide()
+        self.housesGroupBox.hide()
+        self.inJailGroupBox.hide()
+
+        self.normalCardFrame.hide()
+        self.utilityFrame.hide()
+        self.travelSquareFrame.hide()
+
 
