c4503275f40127f36b5ccd9ecc0108dbf0c10280
William Sargent <Sp1riting@users.noreply.github.com>
2024-03-25 08:31:49 +0000

    fixed the rentFromJail and FastBankruptcy not being carried into the main game properly by using the .isChecked() method

--- gameCode.py
+++ gameCode.py
@@ -260,6 +260,7 @@ class GameWindow(QDialog):
                               self.dBlue2lcdNumber
                               ]
         print(rentFromJail)
+        print(fastBankruptcy)
 
         gameValues = values(playerList[0], random.sample([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16], 17))
         self.currentPlayerLabel.setText(playerNames[0])
